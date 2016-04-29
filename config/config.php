e0b7b127779a8ba3eca49e9210827fbe134b4bc0
s-shirayama <shirayama5288@gmail.com>
2016-04-29 23:29:47 +0000

    update config.php

--- config/config.php
+++ config/config.php
@@ -3,6 +3,122 @@
 $config = [];
 
 $config['pipeline_definition'] = [
+	'fbbot' => [
+		'segments' => [
+			1 => [
+				'type' => 'input',
+				'path' => '/imomushi/fbbot',
+				'protocol' => 'http',
+				'method' => 'post',
+			],
+			2 => [
+				'type' => 'function',
+				'function' => 'FacebookRequestParser',
+			],
+			3 => [
+				'type' => 'function',
+				'function' => 'FacebookSendMessage',
+				'config' => [
+					 'access_token' => getenv('FB_ACCESS_TOKEN')
+				]
+			],
+			99 => [
+				'type' => 'output',
+				'protocol' => 'http',
+			]
+		],
+		'dependencies' => [
+			[
+				'from' => 1,
+				'to' => 2,
+			],[
+				'from' => 2,
+				'to' => 3,
+			],[
+				'from' => 1,
+				'to' => 99,
+			]
+		]
+	],
+	'fbbot_verify' => [
+		'segments' => [
+			1 => [
+				'type' => 'input',
+				'path' => '/imomushi/fbbot',
+				'protocol' => 'http',
+			],
+			2 => [
+				'type' => 'function',
+				'function' => 'FBBotVerify',
+				'config' => ['verify_token' => getenv('VERIFY_TOKEN')]
+			],
+			3 => [
+				'type' => 'output',
+				'protocol' => 'http',
+			]
+		],
+		'dependencies' => [
+			[
+				'from' => 1,
+				'to' => 2,
+			],[
+				'from' => 2,
+				'to' => 3,
+			]
+		]
+	],
+	'rakuten' => [
+		'segments' => [
+			1 => [
+				'type' => 'input',
+				'path' => '/imomushi/rakuten',
+				'protocol' => 'http',
+				'method' => 'post',
+			],
+			2 => [
+				'type' => 'function',
+				'function' => 'LineRequestParser',
+			],
+			3 => [
+				'type' => 'function',
+				'function' => 'RakutenApi',
+				'config' => [
+					 'application_id'     => getenv('RAKUTEN_APPLICATION_ID'),
+				]
+			],
+			4 => [
+				'type' => 'function',
+				'function' => 'LineApi',
+				'config' => [
+					 'line_channel_id'     => getenv('LINE_CHANNEL_ID'),
+					 'line_channel_secret' => getenv('LINE_CHANNEL_SECRET'),
+					 'line_channel_mid'    => getenv('LINE_CHANNEL_MID'),
+				]
+			],
+			5 => [
+				'type' => 'output',
+				'protocol' => 'http',
+			]
+		],
+		'dependencies' => [
+			[
+				'from' => 1,
+				'to' => 2,
+			],[
+				'from' => 2,
+				'to' => 3,
+			],[
+				'from' => 3,
+				'to' => 4,
+			],[
+				'from' => 2,
+				'to' => 4,
+			],[
+				'from' => 1,
+				'to' => 5,
+			]
+		]
+	],
 	'repeat' => [
 		'segments' => [
 			1 => [
@@ -12,11 +128,11 @@ $config['pipeline_definition'] = [
 			],
 			2 => [
 				'type' => 'function',
-				'function' => 'ParseLineInput',
+				'function' => 'LineRequestParser',
 			],
 			3 => [
 				'type' => 'function',
-				'function' => 'CallLineBotApi',
+				'function' => 'LineEchoBack',
 				'config' => [
 					 'line_channel_id'     => getenv('LINE_CHANNEL_ID'),
 					 'line_channel_secret' => getenv('LINE_CHANNEL_SECRET'),
